0451b50272d11d1509dca92750d3400c06f0add8
pezemiwe <pezemiwe@gmail.com>
2022-03-08 23:59:33 +0100

    config

--- src/Config.ts
+++ src/Config.ts
@@ -2,12 +2,11 @@ const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 
 const config = {
     appId: CLIENT_ID || '',
-    redirectUri: 'http://localhost:3000',
+    redirectUri: 'http://localhost:3000/',
     scopes: [
       'user.read',
       'mail.readwrite',
       'mailboxsettings.read',
-      'calendars.readwrite'
     ]
   };
   
